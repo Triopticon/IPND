d91e37778a676aac85a117e14c721d38e5540311
Kenneth Soerensen <kenneth@ks81.com>
2016-08-24 02:14:02 +0200

    Initial commit of Break Time

--- Stage 3/break_time.py
+++ Stage 3/break_time.py
@@ -1,2 +1,16 @@
+import time
 import webbrowser
-webbrowser.open("http://www.nrk.no")
+
+total_breaks = 3
+break_count = 0
+
+print "This program started on " + time.ctime()
+
+while break_count < total_breaks: 
+    break_count += 1
+    print "Start sleep, round: " + str(break_count) + " of 3 @ " + time.ctime()
+    time.sleep(15)
+    print "End sleep, round: " + str(break_count) + " of 3 @ " + time.ctime()
+    webbrowser.open("https://radio.nrk.no/direkte/mp3")
+
+print "This program ended on " + time.ctime()
